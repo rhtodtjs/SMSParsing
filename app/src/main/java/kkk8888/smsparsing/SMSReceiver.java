1817ae981947fbdb2ecba219ea68fb5164fc1633
kkk8888 <bjh0977@naver.com>
2017-09-29 14:25:16 +0900

    FUCK

--- app/src/main/java/kkk8888/smsparsing/SMSReceiver.java
+++ app/src/main/java/kkk8888/smsparsing/SMSReceiver.java
@@ -75,36 +75,44 @@ public class SMSReceiver extends BroadcastReceiver {
             if (temp == null)
                 Toast.makeText(context, "Unsaved Phone Number", Toast.LENGTH_SHORT).show();
 
-            if (sender.contains(temp)) {
-                queue = Volley.newRequestQueue(context);
-                qc = context;
-                SimpleMultiPartRequest smpr = new SimpleMultiPartRequest(Request.Method.POST, php, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+            String[] fuck = temp.split(",");
 
-                        Toast.makeText(qc, "저장 완료", Toast.LENGTH_SHORT).show();
-                        Log.i("확인해", response);
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(qc, "에러.." + error, Toast.LENGTH_SHORT).show();
-                        Log.i("에러메세지", error + "");
-                    }
-                });
+            queue = Volley.newRequestQueue(context);
+            qc = context;
 
 
-                smpr.addStringParam("number", sender);
-                smpr.addStringParam("message", message);
+            for (int i = 0; i < fuck.length; i++) {
+                Log.i("키키", fuck[i]);
 
-                smpr.addStringParam("uri", temp1);
-                smpr.addStringParam("user", temp2);
-                smpr.addStringParam("pass", temp3);
-                smpr.addStringParam("dbname", temp4);
-                smpr.addStringParam("port", temp5);
+                if (sender.contains(fuck[i])) {
 
-                queue.add(smpr);
+                    SimpleMultiPartRequest smpr = new SimpleMultiPartRequest(Request.Method.POST, php, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            Toast.makeText(qc, "저장 완료", Toast.LENGTH_SHORT).show();
+                            Log.i("확인해", response);
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(qc, "에러.." + error, Toast.LENGTH_SHORT).show();
+                            Log.i("에러메세지", error + "");
+                        }
+                    });
+
+
+                    smpr.addStringParam("number", sender);
+                    smpr.addStringParam("message", message);
+
+                    smpr.addStringParam("uri", temp1);
+                    smpr.addStringParam("user", temp2);
+                    smpr.addStringParam("pass", temp3);
+                    smpr.addStringParam("dbname", temp4);
+                    smpr.addStringParam("port", temp5);
+
+                    queue.add(smpr);
 
 
 //                new Thread() {
@@ -154,7 +162,10 @@ public class SMSReceiver extends BroadcastReceiver {
 //                    }
 //                }.start();
 
+                }
             }
+
+
         } else if (MainActivity.USER_DEFINED_MSG.equals(intent.getAction())) { // 사용자가 요구한 경우
 
             Toast.makeText(context, intent.getStringExtra("data"), Toast.LENGTH_SHORT).show();
